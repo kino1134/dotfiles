c87ae3f02834826cccc8af46c50131f32cdef1c8
kino1134 <kiino1134@gmail.com>
2018-11-28 20:34:23 +0900

    ssh設定ファイルの配置コマンドを追加

--- init.sh
+++ init.sh
@@ -6,3 +6,7 @@ mv ~/.bashrc ~/bashrc_bk
 ln bash_profile.sh ~/.bash_profile
 ln bashrc.sh ~/.bashrc
 [ -f bash_local.sh ] && ln bash_local.sh ~/.bash_local
+
+[ -f id_rsa ] && ln id_rsa ~/.ssh/id_rsa
+[ -f id_rsa.pub ] && ln id_rsa.pub ~/.ssh/id_rsa.pub
+[ -f ssh_config ] && ln ssh_config ~/.ssh/config
